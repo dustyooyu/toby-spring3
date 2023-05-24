1b9b6869d8ce2d089a3d6b86504dac99f9314101
dustyooyu <somecalm@gmail.com>
2023-05-24 15:37:56 +0900

    feat. UserDaoTest 분리

---
+++ src/main/java/dao/UserDaoTest.java
@@ -0,0 +1,23 @@
+package dao;
+
+import domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+        UserDao userDao = new UserDao(connectionMaker);
+        User user = new User();
+        user.setId("7");
+        user.setName("일곱번째");
+        user.setPassword("1234");
+        userDao.add(user);
+
+        User selectedUser = userDao.get("7");
+        System.out.println(selectedUser.getId());
+        System.out.println(selectedUser.getName());
+        System.out.println(selectedUser.getPassword());
+    }
+}
+
